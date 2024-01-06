5443fefba3d41d497a0b34862dd169b25c4c316c
Zheng Kai <zhengkai@gmail.com>
2024-01-07 01:07:39 +0800

    up

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -39,4 +39,12 @@ return require('packer').startup(function()
 
 	use {'neoclide/coc.nvim', branch = 'release'}
 
+	use {
+		"pmizio/typescript-tools.nvim",
+		requires = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		config = function()
+			require("typescript-tools").setup {}
+		end,
+	}
+
 end)
