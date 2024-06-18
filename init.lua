806d3f2fe23fa065f2d4625489ef45b176254647
Zheng Kai <zhengkai@gmail.com>
2024-06-18 11:51:23 +0800

    up

---
+++ init.lua
@@ -0,0 +1,17 @@
+require('z')
+-- require('plugin.coc')
+-- require('plugin.misc')
+
+vim.cmd('source ~/.config/nvim/misc.vim')
+vim.cmd('source ~/.config/nvim/backup.vim')
+vim.cmd('source ~/.config/nvim/make.vim')
+vim.cmd('source ~/.config/nvim/hotkey.vim')
+vim.cmd('source ~/.config/nvim/status.vim')
+vim.cmd('source ~/.config/nvim/format.vim')
+vim.cmd('source ~/.config/nvim/coc.vim')
+vim.cmd('source ~/.config/nvim/fzf.vim')
+vim.cmd('source ~/.config/nvim/pair-complete.vim')
+vim.cmd('source ~/.config/nvim/style.vim')
+vim.cmd('source ~/.config/nvim/autocmd.vim')
+
+require('config')
---
+++ lua/z.lua
@@ -0,0 +1,69 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", 
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{
+	    'neoclide/coc.nvim',
+	    branch = 'release'
+	},
+	{ 'nvim-treesitter/nvim-treesitter',
+		run = function()
+			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+			ts_update()
+		end
+	},
+	{ 'nvim-treesitter/playground', after = 'nvim-treesitter' },
+
+	-- { 'haishanh/night-owl.vim' },
+	{ 'tomasiser/vim-code-dark' },
+	{ 'itchyny/lightline.vim' },
+
+	-- { 'fatih/vim-go' },
+
+	{ 'tpope/vim-fugitive' },
+
+	-- { 'liuchengxu/vista.vim' },
+
+	{ 'vim-scripts/YankRing.vim' },
+
+	-- { 'python-lsp/python-lsp-server' },
+
+	{ 'chrisbra/csv.vim' },
+
+	{ 'tomtom/tcomment_vim' },
+
+	{ 'https://git.sr.ht/~whynothugo/lsp_lines.nvim' },
+
+	-- { 'dense-analysis/ale' },
+
+	{ 'mattn/emmet-vim' },
+
+	{ 'easymotion/vim-easymotion' },
+
+	-- { 'junegunn/fzf', 
+	-- 	build = function() 
+	-- 		vim.fn['fzf#install()']()
+	-- 	end 
+	-- },
+	{ 'junegunn/fzf.vim' },
+
+	-- { 'eandrju/cellular-automaton.nvim' },
+
+	{ 'pmizio/typescript-tools.nvim',
+		dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+		config = function()
+			require("typescript-tools").setup {}
+		end
+	},
+})
