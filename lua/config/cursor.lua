052088fea3f94ffbb55c20dbbbe190b4858f93dc
Zheng Kai <zhengkai@gmail.com>
2025-02-14 21:36:04 +0800

    cursor

---
+++ lua/config/cursor.lua
@@ -0,0 +1,12 @@
+-- 光标恢复到上次打开的位置
+
+-- 位置信息参见 :marks
+--
+-- 获取位置为 local row, col = unpack(vim.api.nvim_buf_get_mark(0, '"'))
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+    pattern = "*",
+    callback = function()
+        vim.cmd('normal g`"')
+    end
+})
