cdd3328d68832fa33b686acdbd47ab73002db64f
Zheng Kai <zhengkai@gmail.com>
2025-02-22 16:16:07 +0800

    up

--- lua/lsp/golang.lua
+++ lua/lsp/golang.lua
@@ -23,7 +23,7 @@ local function get_doc_link()
 	end)
 end
 
-function GoImports(bufnr)
+local function goImports(bufnr)
 	local bufname = vim.api.nvim_buf_get_name(bufnr)
 
 	-- 使用系统的命令调用 goimports，并捕获输出（标准输出和错误）
@@ -38,6 +38,24 @@ function GoImports(bufnr)
 	-- 将结果写回到缓冲区
 	local lines = vim.split(result, "\n")
 	vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+end
+
+local function syncOrganizeImports()
+	local params = vim.lsp.util.make_range_params()
+	params.context = { only = { "source.organizeImports" } }
+	-- 使用同步方法，结果在 1000 毫秒内返回
+	local response = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 1000)
+
+	-- 解析结果并应用编辑
+	for _, result in pairs(response or {}) do
+		for _, r in pairs(result.result or {}) do
+			if r.edit then
+				vim.lsp.util.apply_workspace_edit(r.edit, "utf-8")
+			elseif r.command then
+				vim.lsp.buf.execute_command(r.command)
+			end
+		end
+	end
 end
 
 require("lspconfig").gopls.setup({
@@ -56,7 +74,8 @@ require("lspconfig").gopls.setup({
 			buffer = bufnr,
 			callback = function()
 				vim.lsp.buf.format({ async = false })
-				GoImports(bufnr)
+				-- GoImports(bufnr)
+				syncOrganizeImports()
 			end,
 		})
 	end,
